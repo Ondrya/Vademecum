79e3c64ee9ef8366fc995f59d78735ce8b169f97
Andrew Evsugin <ondrya@yandex.ru>
2021-05-17 15:48:34 +0300

    fix changing selected LaType

--- WpfApp/Helpers/Dict.cs
+++ WpfApp/Helpers/Dict.cs
@@ -13,32 +13,52 @@ namespace WpfApp.Helpers
     /// </summary>
     public class Dict
     {
-        private static string cn = ((App)Application.Current).CurrentDb.ToString();
-        private static int laType = (int)((App)Application.Current).CurrentSession.SelectedLaType;
+        /// <summary>
+        /// Получить строку соединения
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCn()
+        {
+            return ((App)Application.Current).CurrentDb.ToString();
+        }
+
+
+        /// <summary>
+        /// Текущий тип ЛА
+        /// </summary>
+        /// <returns></returns>
+        public static int GetSelectedLaType()
+        {
+            return (int)((App)Application.Current).CurrentSession.SelectedLaType;
+        }
 
         /// <summary>
         /// Полуить список типов устройств
         /// </summary>
         /// <returns></returns>
-        public static List<DeviceType> GetDeviceTypes() => new DataContext(cn).DeviceTypes.ToList();
-        public static List<DataLayer.Type> GetTypes() => new DataContext(cn).Types.ToList();
-        public static List<Function> GetFunctions() => new DataContext(cn).Functions.ToList();
-        public static List<Sens_Element> GetSensElements() => new DataContext(cn).Sens_Element.ToList();
-        public static List<Kind> GetKinds() => new DataContext(cn).Kinds.ToList();
-        public static List<Control> GetControls() => new DataContext(cn).Controls.ToList();
-        public static List<Measure_Processing> GetMeasureProcessing() => new DataContext(cn).Measure_Processing.ToList();
-        public static List<BuiltTech> GetBuiltTeches() => new DataContext(cn).BuiltTeches.ToList();
-        public static List<Measure> GetMeasures() => new DataContext(cn).Measures.ToList();
-        public static List<Measure_Dims> GetMeasureDims() => new DataContext(cn).Measure_Dims.ToList();
-        public static List<Enviroment> GetEnviroments() => new DataContext(cn).Enviroments.ToList();
+        public static List<DeviceType> GetDeviceTypes() => new DataContext(GetCn()).DeviceTypes.ToList();
+        public static List<DataLayer.Type> GetTypes() => new DataContext(GetCn()).Types.ToList();
+        public static List<Function> GetFunctions() => new DataContext(GetCn()).Functions.ToList();
+        public static List<Sens_Element> GetSensElements() => new DataContext(GetCn()).Sens_Element.ToList();
+        public static List<Kind> GetKinds() => new DataContext(GetCn()).Kinds.ToList();
+        public static List<Control> GetControls() => new DataContext(GetCn()).Controls.ToList();
+        public static List<Measure_Processing> GetMeasureProcessing() => new DataContext(GetCn()).Measure_Processing.ToList();
+        public static List<BuiltTech> GetBuiltTeches() => new DataContext(GetCn()).BuiltTeches.ToList();
+        public static List<Measure> GetMeasures() => new DataContext(GetCn()).Measures.ToList();
+        public static List<Measure_Dims> GetMeasureDims() => new DataContext(GetCn()).Measure_Dims.ToList();
+        public static List<Enviroment> GetEnviroments() => new DataContext(GetCn()).Enviroments.ToList();
        
-        public static List<Producer> GetProdusers() => new DataContext(cn).Producers.ToList();
-        public static List<Literature> GetLiterature() => new DataContext(cn).Literatures.ToList();
+        public static List<Producer> GetProdusers() => new DataContext(GetCn()).Producers.ToList();
+        public static List<Literature> GetLiterature() => new DataContext(GetCn()).Literatures.ToList();
 
-        public static List<int> GetDeviceEnviroments(int currentDeviceId) => new DataContext(cn).Devices.Find(currentDeviceId)?.Enviroments.Select(x => x.id_envi).ToList();
-        public static List<int> GetDeviceLiteratures(int currentDeviceId) => new DataContext(cn).Devices.Find(currentDeviceId)?.Literatures.Select(x => x.id_lit).ToList();
+        public static List<int> GetDeviceEnviroments(int currentDeviceId) => new DataContext(GetCn()).Devices.Find(currentDeviceId)?.Enviroments.Select(x => x.id_envi).ToList();
+        public static List<int> GetDeviceLiteratures(int currentDeviceId) => new DataContext(GetCn()).Devices.Find(currentDeviceId)?.Literatures.Select(x => x.id_lit).ToList();
         //public static List<int> GetDeviceMeasures(int currentDeviceId) => new DataContext(cn).Devices.Find(currentDeviceId)?.Measures.Select(x => x.id_measure).ToList();
-     
-        public static List<Device> GetDevices() => new DataContext(cn).Devices.Where(x => x.id_LA == laType).ToList();
+
+        public static List<Device> GetDevices()
+        {
+            var laType = GetSelectedLaType();
+            return new DataContext(GetCn()).Devices.Where(x => x.id_LA == laType).ToList();
+        }
     }
 }
